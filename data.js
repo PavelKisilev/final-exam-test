5b5397fa86fbe39e516906f8cbe07ec34ea66233
PavelName <95@ya.ru>
2023-04-10 22:14:21 +0300

    03

--- data.js
+++ data.js
@@ -9,6 +9,12 @@ const dataInfo = `[
     "h1": "Fully customizable to address your needs ",
     "p": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar sit amet.",
     "img": "./IMG/screen-02.svg" 
+},
+
+{
+    "h1": "Pre-built Dashboard Templates",
+    "p": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Elementum nisi aliquet volutpat pellentesque volutpat est. Sapien in etiam vitae nibh nunc mattis imperdiet sed nullam. Vitae et, tortor pulvinar risus pulvinar sit amet.",
+    "img": "./IMG/screen-03.svg" 
 }
 
 ]`;
--- script.js
+++ script.js
@@ -1,29 +1,41 @@
 const data = JSON.parse(dataInfo);
+const container = document.querySelector(".container02");
 
-const base = document.querySelector(".card");
-const baseorder1 = document.querySelector(".cardtext");
-const baseorder2 = document.querySelector(".cardSvgImage");
-
-
-
-data.reverse().forEach((element) => {
-    
-  base.insertAdjacentHTML(
-    "afterend",
-    `
-    <div class="card">
+data.forEach((element,i) => {
+  const newCard = document.createElement("div");
+  newCard.classList.add("card");
+  newCard.innerHTML = `
         <div class="cardtext">
             <h1 class="cardtexth1">${element.h1}</h1>            
             <p class="cardtextP">${element.p}</p>
         </div>
-            <img
+        <img
             class="cardSvgImage"
             src="${element.img}"
-            alt="изображение недоступно"/>            
-    </div>
-    `
-  );
-  baseorder1.classList.toggle('_reverse');
-  baseorder2.classList.toggle('_reverse');
+            alt="изображение недоступно"/> 
+    `;
+    if ((i+1) % 2 === 0) 
+    {
+        newCard.querySelector(".cardtext").classList.add("reverse");
+        newCard.querySelector(".cardSvgImage").classList.add("reverse");
+    }
+    container.appendChild(newCard)
 });
 
+// data.reverse().forEach((element, i) => {
+//   base.insertAdjacentHTML(
+//     "afterend",
+//     `
+    // <div class="card">
+    //     <div class="cardtext">
+    //         <h1 class="cardtexth1">${element.h1}</h1>            
+    //         <p class="cardtextP">${element.p}</p>
+    //     </div>
+    //         <img
+    //         class="cardSvgImage"
+    //         src="${element.img}"
+    //         alt="изображение недоступно"/>            
+    // </div>
+//     `
+//   );
+// });
